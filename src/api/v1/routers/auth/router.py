7442d5d08695ca0953da33dbde2b8a381ba8ce3a
Paul Nyk <nyoikepaul2@gmail.com>
2026-04-22 21:03:42 +0300

    refactor: suppress bandit false positive for bearer token type

--- src/api/v1/routers/auth/router.py
+++ src/api/v1/routers/auth/router.py
@@ -54,5 +54,5 @@ def login(
     return {
         "access_token": access_token,
         "refresh_token": refresh_token,
-        "token_type": "bearer",
+         "token_type": "bearer ", # nosec B105
     }
